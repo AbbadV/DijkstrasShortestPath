df876580196d16a0acf525b3eed366f8671767b9
Shyawn Karim <shyawnkarim@yahoo.com>
2015-10-27 16:17:21 -0700

    graphm.h partially defined

--- graphm.h
+++ graphm.h
@@ -6,9 +6,37 @@
 #define DIJKSTRASSHORTESTPATH_GRAPHM_H
 
 
+#include "nodedata.h"
+
+const int MAXNODES = 100;
+
 class GraphM
 {
 public:
+    GraphM();
+    GraphM(const GraphM &);
+    ~GraphM();
+
+    // builds up graph node information and adjacency matrix of edges
+    // between each node reading from a data file
+    void buildGraph();
+
+    // insert an edge into graph between two given nodes
+    void insertEdge();
+
+    // remove an edge between two given nodes
+    void removeEdge();
+
+    // find the shortest path between every node to every other node in
+    // the graph, i.e., TableType T is updated with shortest path information
+    void findShortestPath();
+
+    // display the cost and path from every node to every other node
+    void displayAll();
+
+    // uses couts to display the shortest distance with path info between
+    // the fromNode to toNode
+    void display();
 
 
 private:
@@ -23,8 +51,6 @@ private:
     int C[MAXNODES][MAXNODES];          // cost array, the adjacency matrix
     int size;                           // number of nodes in the graph
     TableType T[MAXNODES][MAXNODES];    // stores visited, distance, path
-
-
 };
 
 
